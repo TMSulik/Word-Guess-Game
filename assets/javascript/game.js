4024c9564dcf55f8a5081c565839e604d8ec6257
TMSulik <shulich@hotmail.com>
2018-12-18 20:18:54 -0800

    First draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+var philosphers = {
+  Empedocles: "Deduced that the universe is composed of four elements: Earth, Water, Air, and Fire.",
+  "Zeno of Elea": "Argued that motion is impossible, using the case of an arrow in flight.",
+  Parmenides: "Proved that the whole universe as a whole is timeless and unified, and that change on the macro level is impossible.",
+  Protagoras: "Since “man is the measure of all things,” all that can exist to us are subjective interpretations, so  what is true for one individual may be untrue for another.",
+  Gorgias: "The original nihilist, he logically demonstrated that nothing can exist.",
+  Anaxagoras: "He was imprisoned for impiety in Athens for asserting that all events have naturalistic explanations, rather than being caused by the gods.",
+  Heraclitus: "As everything is a state of flux, you cannot step into the same river twice: the river will have changed, and will you have charged as well.", 
+  "Thales of Miletus": "The first to study states electricity by rubbing threads of fiber on amber, and to get rich by predicting bumper crop years using logic.",
+  Democritus:
+  "Known as the Laughing Philosopher, he deduced that the universe consisted of atoms and vacuums at a fundamental level.",
+  Pythagoras:
+  "Devised formulas for calculating the sides of geometric shapes and solids, while founding a reincarnation cult." 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
